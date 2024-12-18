2cf0d03ec410743eea8a3a1bc0be40a9a4c1b213
ummesalma303 <ummesalma333999@gmail.com>
2024-12-18 09:38:45 +0600

    Added count api

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
-
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ot76b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -30,6 +30,13 @@ async function run() {
 
     const productCollection = client.db('emaJohnDB').collection('products');
 
+    // pagination
+    app.get("/productsCount",async(req,res)=>{
+      const count = await productCollection.estimatedDocumentCount()
+      // console.log(count)
+      // const result = await productCollection
+      res.send({count})
+    })
     app.get('/products', async(req, res) => {
         const result = await productCollection.find().toArray();
         res.send(result);
